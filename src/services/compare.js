204a7485c52d871fc82afbd849fae11cae7679c4
chrisloarryn <chrisloarryn@gmail.com>
2021-05-01 20:22:41 -0400

    feat: better list sorter

--- src/services/compare.js
+++ src/services/compare.js
@@ -8,3 +8,38 @@ export const compareUsers = (a, b) => {
   }
   return 0
 }
+
+export const sortArrayByProperty = (
+  array,
+  property,
+  type = 'number',
+  direction = 1
+) => {
+  if (type === 'date')
+    return array.sort(
+      (a, b) =>
+        (new Date(a[property]).getTime() - new Date(b[property]).getTime()) *
+        direction
+    )
+
+  if (type === 'string')
+    return array.sort((a, b) => {
+      const valA = a[property],
+        valB = b[property]
+
+      if (typeof valA === 'string' && typeof valB === 'string')
+        return valA.localeCompare(valB) * direction
+
+      return 0
+    })
+
+  return array.sort((a, b) => {
+    const valA = a[property],
+      valB = b[property]
+
+    if (typeof valA === 'number' && typeof valB === 'number')
+      (valA - valB) * direction
+
+    return 0
+  })
+}
